20030de966a28945e4e1bc38a81ce87c98d53d5c
aaron <kaven22314567@gmail.com>
2022-10-19 14:16:01 +0800

    [feat] update basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter basename={process.env.REACT_APP_BASE_URL}>
+  <BrowserRouter basename="/aaron_website">
     <App />
   </BrowserRouter>
   // </React.StrictMode>
